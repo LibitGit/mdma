8ab26c2f9740a54972d3c5f747e287c1631b1d95
Libit <dawidobaldyga1@gmail.com>
2024-06-17 21:12:53 +0200

    Fixed incorrect caching

--- extension/foreground.js
+++ extension/foreground.js
@@ -1,4 +1,4 @@
 (async () => {
-    const {default: init} = await import("https://libit.ovh/mdma/foreground.js");
-    await init();
+    const {default: init} = await import(`https://libit.ovh/mdma/foreground.js?c=${Date.now()}`);
+    await init(`https://libit.ovh/mdma/foreground_bg.wasm?c=${Date.now()}`);
 })();
