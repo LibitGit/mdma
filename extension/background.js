967abca02a3c78ae108d6225c6b286a19b6a3e64
Libit <dawidobaldyga1@gmail.com>
2025-01-24 22:32:21 +0100

    MDMA 0.11.0-test

--- extension/background.js
+++ extension/background.js
@@ -1,18 +1,2 @@
-import __wbg_init, { handlePortConnect, handleActionClick, handleMessage, handleTabsUpdated } from "https://libit.ovh/mdma/background.js";
+import * as background from "https://www.libit.ovh/mdma/background_js.js"
 
-let module_or_path = {module_or_path: `https://libit.ovh/mdma/background_bg.wasm.br?c=${Date.now()}`};
-let init = new Promise((res, rej) => __wbg_init(module_or_path).then(res).catch(rej));
-
-init.catch(console.error)
-
-let jsHandlePortConnect = port => init.then(() => handlePortConnect(port).catch(console.error)).catch(console.error);
-let jsHandleMessage = (message, sender, sendResponse) => init.then(() => handleMessage(message, sender, sendResponse).catch(console.error)).catch(console.error);
-let jsHandleActionClick = tab => init.then(() => handleActionClick(tab).catch(console.error)).catch(console.error);
-let jsHandleTabsUpdated = (tabId, changeInfo, tab) => init.then(() => handleTabsUpdated(tabId, changeInfo, tab).catch(console.error)).catch(console.error);
-
-chrome.runtime.onConnect.addListener(jsHandlePortConnect);
-chrome.runtime.onConnectExternal.addListener(jsHandlePortConnect);
-chrome.runtime.onMessage.addListener(jsHandleMessage);
-chrome.runtime.onMessageExternal.addListener(jsHandleMessage);
-chrome.action.onClicked.addListener(jsHandleActionClick);
-chrome.tabs.onUpdated.addListener(jsHandleTabsUpdated);
---
+++ extension/popup_js.js
@@ -0,0 +1,2 @@
+import{_ as e,e as a}from"./index-BmWsMfXY.js";const s=new URL("assets/popup-CUcwwp7T.wasm",import.meta.url);let m=new Promise((async m=>{m(await async function(s){return await e({module_or_path:await s.module,memory:s.memory}),a}({module:s}))}));chrome.runtime.onMessage.addListener((async(e,a,s)=>(await m).handleMessage(e,a,s)));
+//# sourceMappingURL=popup_js.js.map
